91bd4ee7be51f0aa1e91a58d459835b5eebe069d
Andres Ruiz <andreu.chan@gmail.com>
2022-10-24 10:27:54 +0900

    Added AnimatedContent to animate transition between TopAppBar and SearchAppBar.

--- app/src/main/java/com/are/magicboxtwo/ui/common/topappbar/MagicBoxTopAppBar.kt
+++ app/src/main/java/com/are/magicboxtwo/ui/common/topappbar/MagicBoxTopAppBar.kt
@@ -1,5 +1,9 @@
 package com.are.magicboxtwo.ui.common.topappbar
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.TweenSpec
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
@@ -15,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.are.magicboxtwo.R
@@ -27,7 +32,16 @@ enum class ActionType {
     LIGHT_DARK
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+internal val SlideAnimationSpec: FiniteAnimationSpec<IntOffset> =
+    TweenSpec(300, 0, LinearEasing)
+
+internal val FadeAnimationSpec: FiniteAnimationSpec<Float> =
+    TweenSpec(300, 0, LinearEasing)
+
+@OptIn(
+    ExperimentalMaterial3Api::class,
+    ExperimentalAnimationApi::class
+)
 @Composable
 fun MagicBoxTopAppBar(
     navController: NavHostController,
@@ -47,76 +61,99 @@ fun MagicBoxTopAppBar(
         else -> true
     }
 
-    when (uiState.searchState) {
-        SearchState.Searching -> {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(64.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                SearchAppBar(
-                    onSearch = {
-                        onSearch(it)
-                    },
-                    onClear = onSearchClose
-                )
-            }
+    AnimatedContent(
+        modifier = Modifier
+            .fillMaxWidth(),
+        targetState = uiState.searchState,
+        transitionSpec = {
+            slideInHorizontally(
+                animationSpec = SlideAnimationSpec,
+                initialOffsetX = {
+                    if (uiState.searchState == SearchState.Searching) it else -it
+                }
+            ) + fadeIn(
+                animationSpec = FadeAnimationSpec
+            ) with slideOutHorizontally(
+                animationSpec = SlideAnimationSpec,
+                targetOffsetX = {
+                    if (uiState.searchState == SearchState.Searching) -it else it
+                }
+            ) + fadeOut(
+                animationSpec = FadeAnimationSpec
+            )
         }
-        SearchState.Closed -> {
-            TopAppBar(
-                modifier = Modifier
-                    .background(
-                        color = MBTheme.colors.onPrimary
-                    ),
-                title = {
-                    Text(
-                        text = if (showBackButton) uiState.appBarTitle else stringResource(id = R.string.app_name),
-                        color = MBTheme.colors.primary
+    ) { state ->
+        when (state) {
+            SearchState.Searching -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(64.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    SearchAppBar(
+                        onSearch = {
+                            onSearch(it)
+                        },
+                        onClear = onSearchClose
                     )
-                },
-                navigationIcon = {
-                    IconButton(
-                        onClick = if (showBackButton) onBackClick else onDrawerClick,
-                        content = {
-                            Icon(
-                                imageVector = if (showBackButton) Icons.Default.ArrowBack else Icons.Default.Menu,
-                                contentDescription = "Toggle Drawer"
+                }
+            }
+            SearchState.Closed -> {
+                TopAppBar(
+                    modifier = Modifier
+                        .background(
+                            color = MBTheme.colors.onPrimary
+                        ),
+                    title = {
+                        Text(
+                            text = if (showBackButton) uiState.appBarTitle else stringResource(id = R.string.app_name),
+                            color = MBTheme.colors.primary
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = if (showBackButton) onBackClick else onDrawerClick,
+                            content = {
+                                Icon(
+                                    imageVector = if (showBackButton) Icons.Default.ArrowBack else Icons.Default.Menu,
+                                    contentDescription = "Toggle Drawer"
+                                )
+                            }
+                        )
+                    },
+                    actions = {
+                        if (!showBackButton) {
+                            IconButton(
+                                onClick = {
+                                    onActionClick(ActionType.SEARCH)
+                                },
+                                content = {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.ic_search),
+                                        contentDescription = null
+                                    )
+                                }
+                            )
+                            IconButton(
+                                onClick = {
+                                    onActionClick(ActionType.LIGHT_DARK)
+                                },
+                                content = {
+                                    Icon(
+                                        painter = if (isSystemInDarkTheme()) {
+                                            painterResource(id = R.drawable.ic_dark_mode)
+                                        } else {
+                                            painterResource(id = R.drawable.ic_light_mode)
+                                        },
+                                        contentDescription = null
+                                    )
+                                }
                             )
                         }
-                    )
-                },
-                actions = {
-                    if (!showBackButton) {
-                        IconButton(
-                            onClick = {
-                                onActionClick(ActionType.SEARCH)
-                            },
-                            content = {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.ic_search),
-                                    contentDescription = null
-                                )
-                            }
-                        )
-                        IconButton(
-                            onClick = {
-                                onActionClick(ActionType.LIGHT_DARK)
-                            },
-                            content = {
-                                Icon(
-                                    painter = if (isSystemInDarkTheme()) {
-                                        painterResource(id = R.drawable.ic_dark_mode)
-                                    } else {
-                                        painterResource(id = R.drawable.ic_light_mode)
-                                    },
-                                    contentDescription = null
-                                )
-                            }
-                        )
                     }
-                }
-            )
+                )
+            }
         }
     }
 }
